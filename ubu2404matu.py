cb70871952769d0d5212f3d71d55ab010346b60c
desinfectuser <95765132+desinfectuser@users.noreply.github.com>
2025-01-12 21:17:50 +0100

    Add files via upload

---
+++ ubu2404matu.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.12
+import os
+import sys
+import netifaces
+
+if os.getuid() != 0:
+    print("Fehlende Root-Rechte!")
+    exit(1)
+
+hostname = input("Neuer Hostname: ")
+
+if hostname == "":
+    print("Ungültiger Hostname!")
+    exit(1)
+
+with open("/etc/hostname","w") as file:
+    file.write(hostname)
+
+with open(hostname, "w") as outfile:
+    sys.stdout = outfile
+    print(hostname)
+    print("Mac-Adresse(n):")
+    for link in netifaces.interfaces():
+        print(link, ":", netifaces.ifaddresses(link)[netifaces.AF_LINK])
+
+with open("/etc/default/grub", "w") as grubconf_alt:
+    with open("grub", "r") as grubconf_neu:
+        grubconf_alt.write(grubconf_neu.read())
+
+os.system("update-grub")
