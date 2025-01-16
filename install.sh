d05e4b2055273bbad2e9cccb6c8cd04d98e1ff2e
desinfectuser <95765132+desinfectuser@users.noreply.github.com>
2025-01-16 09:48:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ then
     apt update
     apt full-upgrade -y
     apt autoremove -y
-    apt install htop tree vlc zsh python3-netifaces
+    apt install -y htop tree vlc zsh python3-netifaces
     wget https://raw.githubusercontent.com/desinfectuser/autoinstall/main/ubu2404matu.py -q -O /tmp/ubu2404matu.py
     wget https://raw.githubusercontent.com/desinfectuser/autoinstall/main/grub -q -O /tmp/grub
     python3.12 /tmp/ubu2404matu.py
